f171c610ae751052114ed45630cff0b6d975e49a
Prashant Kumar <prashantnawa123@gmail.com>
2024-07-23 03:15:04 +0530

    cors issue

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
-  const allowedOrigins = ['*']
-  const app = await NestFactory.create(AppModule, { cors: { origin: allowedOrigins } });
+  const allowedOrigins = ['https://octalogic-frontend-one.vercel.app'];
+  const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
   app.setGlobalPrefix('api');
   app.use(bodyParser.json({ limit: '50mb' }));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
