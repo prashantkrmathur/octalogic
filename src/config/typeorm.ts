8631b735d9e61b294916c1b1c86d4c5ce9b29f32
Prashant Kumar <prashantnawa123@gmail.com>
2024-07-22 10:52:25 +0530

    typeorm config

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -5,20 +5,16 @@ import { DataSource, DataSourceOptions } from "typeorm";
 dotenvConfig({ path: '.env' });
 
 const config = {
-        type: 'postgres',
-    host: `${process.env.PGHOST}`,
-    port: parseInt(`${process.env.PGPORT}`, 10),
-    username: `${process.env.PGUSER}`,
-    password: `${process.env.PGPASSWORD}`,
-    database: `${process.env.PGDATABASE}`,
+    type: 'postgres',
+    host: `${process.env.DATABASE_HOST}`,
+    port: `${process.env.DATABASE_PORT}`,
+    username: `${process.env.DATABASE_USERNAME}`,
+    password: `${process.env.DATABASE_PASSWORD}`,
+    database: `${process.env.DATABASE_NAME}`,
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
     migrations: ["dist/migrations/*{.ts,.js}"],
     autoLoadEntities: true,
-    synchronize: false,  
-    logging: false, 
-    ssl: {
-        rejectUnauthorized: false
-    }
+    synchronize: true,
 }
 
 
