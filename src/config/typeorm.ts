dcdd3272b25bf8cbe3e9b6191891ba9040b83c5d
Prashant Kumar <prashantnawa123@gmail.com>
2024-07-22 12:17:40 +0530

    typeorm config for deployment

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -6,8 +6,8 @@ dotenvConfig({ path: '.env' });
 
 const config = {
     type: 'postgres',
-    host: `${process.env.DATABASE_HOST}`,
-    port: `${process.env.DATABASE_PORT}`,
+    url: `${process.env.DATABASE_HOST}`,
+    // port: `${process.env.DATABASE_PORT}`,
     username: `${process.env.DATABASE_USERNAME}`,
     password: `${process.env.DATABASE_PASSWORD}`,
     database: `${process.env.DATABASE_NAME}`,
